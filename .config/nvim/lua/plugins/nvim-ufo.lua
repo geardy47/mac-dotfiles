5e4d6d47f45d8445f0fd18b905a5de6ddce16a55
geardy47 <muhammadgenta14@gmail.com>
2025-04-21 09:44:47 +0700

    feat(nvim): add specific close fold keymaps

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -10,6 +10,40 @@ return {
         return { "lsp", "indent" }
       end,
     },
+    keys = {
+      {
+        "z1m",
+        function()
+          require("ufo").closeFoldsWith(1)
+        end,
+        desc = "Close folds with 1",
+        mode = "n",
+      },
+      {
+        "z2m",
+        function()
+          require("ufo").closeFoldsWith(2)
+        end,
+        desc = "Close folds with 2",
+        mode = "n",
+      },
+      {
+        "z3m",
+        function()
+          require("ufo").closeFoldsWith(3)
+        end,
+        desc = "Close folds with 3",
+        mode = "n",
+      },
+    },
+    -- config = function()
+    --   vim.keymap.set("n", "z2m", function()
+    --     require("ufo").closeFoldsWith(2)
+    --   end, { desc = "Close fold with 2" })
+    --   vim.keymap.set("n", "z3m", function()
+    --     require("ufo").closeFoldsWith(3)
+    --   end, { desc = "Close fold with 3" })
+    -- end,
     dependencies = {
       "kevinhwang91/promise-async",
     },
